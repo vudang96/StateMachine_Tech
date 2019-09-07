9e2687f961d9ef7416a8e8b9238fc1955cb088a4
vudang96 <vudang.20142015@gmail.com>
2019-09-07 14:07:02 +0700

    string

--- ex_01.cpp
+++ ex_01.cpp
@@ -1,10 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+void print_reverse(char *s)
+{
+    size_t len = strlen(s);
+
+    char *t = s + len - 1;
+
+    while( t >= s)
+    {
+        printf("%c",*t);
+
+        t = t - 1;
+    }
+    puts("");
+
+}
 
 int main()
 {
-    char strl[1];
-    puts("input: ");
-    scanf("%s",&strl);
-    printf("Hello  %p",strl[1]);
-    return 0;       
+    char s[] = "JQK";
+    print_reverse(s);
+    return 0;
 }
